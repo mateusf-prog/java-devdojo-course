4acc9d2542f4842b1a13a0439402f74fb4871867
mateusfprog <mateus102006@hotmail.com>
2024-04-23 17:56:51 -0300

    tipos primitivos

---
+++ maratona-java/src/curso/devdojo/maratonajava/introduction/Main.java
@@ -0,0 +1,8 @@
+package curso.devdojo.maratonajava.introduction;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
---
+++ maratona-java/src/curso/devdojo/maratonajava/introduction/TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package curso.devdojo.maratonajava.introduction;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = (int) 10000000000L;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 0020;
+
+        System.out.println("A idade é " + idade + " anos");
+        System.out.println(falso);
+        System.out.println(caractere);
+    }
+}
