3f5daad55c70e82f9222910b43ab796e62785413
mateusfprog <mateus102006@hotmail.com>
2024-04-24 22:49:01 -0300

    sobrecarga de construtores

---
+++ maratona-java/src/curso/devdojo/maratonajava/javacore/introducaoclasses/dominio/Pessoa.java
@@ -0,0 +1,66 @@
+package curso.devdojo.maratonajava.javacore.introducaoclasses.dominio;
+
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private char sexo;
+    private int peso;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, int idade, char sexo) {
+        this();
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    public Pessoa(String nome, int idade, char sexo, int peso) {
+        this(nome, idade, sexo);
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", sexo=" + sexo +
+                ", peso=" + peso +
+                '}';
+    }
+}
---
+++ maratona-java/src/curso/devdojo/maratonajava/javacore/introducaoclasses/dominio/test/PessoaTest.java
@@ -0,0 +1,10 @@
+package curso.devdojo.maratonajava.javacore.introducaoclasses.dominio.test;
+
+import curso.devdojo.maratonajava.javacore.introducaoclasses.dominio.Pessoa;
+
+public class PessoaTest {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("Mateus", 26, 'M', 65);
+        System.out.println(pessoa1);
+    }
+}
