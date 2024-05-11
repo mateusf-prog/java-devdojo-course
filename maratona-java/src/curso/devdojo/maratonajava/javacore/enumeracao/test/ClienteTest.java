1bad6a96bedd583016c5c34639ffd36391f1240d
mateusfprog <mateus102006@hotmail.com>
2024-05-11 08:33:39 -0300

    enumeracao pt.1 - o problema

---
+++ maratona-java/src/curso/devdojo/maratonajava/javacore/enumeracao/test/ClienteTest.java
@@ -0,0 +1,10 @@
+package curso.devdojo.maratonajava.javacore.enumeracao.test;
+
+import curso.devdojo.maratonajava.javacore.enumeracao.dominio.Cliente;
+
+public class ClienteTest {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("Tsubasa", "PESSOA_FISICA");
+        Cliente cliente1 = new Cliente("Tsubasa", "PESSOA_JURIDICA");
+    }
+}
